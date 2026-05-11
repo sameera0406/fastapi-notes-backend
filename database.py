3ded83aa60d8f27e873c2700c01a2afd22d77a57
sameera0406 <rvasa@gitam.in>
2026-05-11 15:21:24 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,9 +5,10 @@ from sqlalchemy.orm import sessionmaker
 
 # Replace with your actual string from Step 1
 SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://postgres:RaghaSameera%401546@db.ldapzmifzmpcopwsnihx.supabase.co:5432/postgres")
-
+if url and url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
 # The engine is the actual connection to Supabase
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={"connect_timeout": 10}, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
