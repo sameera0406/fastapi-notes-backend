c1b9c8c12ff84b175c6b13352daae49f527ab54a
sameera0406 <rvasa@gitam.in>
2026-05-11 13:46:21 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Replace with your actual string from Step 1
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:RaghaSameera%401546@db.ldapzmifzmpcopwsnihx.supabase.co:5432/postgres"
+SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://postgres:RaghaSameera%401546@db.ldapzmifzmpcopwsnihx.supabase.co:5432/postgres")
 
 # The engine is the actual connection to Supabase
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
