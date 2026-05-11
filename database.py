7b03ef03a8a0b0295d14cec090dd880dca7c682d
sameera0406 <rvasa@gitam.in>
2026-05-11 15:07:42 +0530

    Enhance database engine with pool_pre_ping
    Added pool_pre_ping option to the database engine.

--- database.py
+++ database.py
@@ -7,6 +7,9 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://postgres:RaghaSameera%401546@db.ldapzmifzmpcopwsnihx.supabase.co:5432/postgres")
 
 # The engine is the actual connection to Supabase
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, pool_pre_ping=True
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
