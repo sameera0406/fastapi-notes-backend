2a064616fe1c23941947aea08cdcd21893d86a87
sameera0406 <rvasa@gitam.in>
2026-05-12 10:33:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sameera-db.vercel.app/"], # Replace with your actual Vercel URL
+    allow_origins=[
+        "https://sameera-db.vercel.app",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
